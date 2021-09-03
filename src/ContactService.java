c726fbedfbde8d7689f8ae5f3ba00dd0175ae370
Erik Engvall <Erik.Engvall@snhu.edu>
2021-09-02 22:05:04 -0700

    Set up VS Code

--- src/ContactService.java
+++ src/ContactService.java
@@ -1,10 +1,15 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.util.UUID;
 
 public class ContactService {
 
+
 	//Comment to create change for git
-	private String uniqueId;
-	private List<Contact> contactList = new ArrayList<>();
+	String uniqueId;
+	List<Contact> contactList = new ArrayList<>();
+	
+	uniqueId = UUID.randomUUID().toString();
+	System.out.println(uniqueId);
 	
 }
