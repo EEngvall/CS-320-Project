403a03b793ceb2339e6772252aa3a220a5613049
Erik Engvall <erik.engvall@snhu.edu>
2021-09-02 20:31:56 -0700

    Initial Commit

---
+++ src/Contact.java
@@ -0,0 +1,66 @@
+
+public class Contact {
+
+	//Comments
+	private String contactId;
+	private String firstName;
+	private String lastName;
+	private String phoneNumber;
+	private String address;
+	private int CONTACT_ID_LENGTH = 10;
+	private int CONTACT_FIRST_NAME_LENGTH = 10;
+	private int CONTACT_LAST_NAME_LENGTH = 10;
+	private int CONTACT_PHONE_LENGTH = 10;
+	private int CONTACT_ADDRESS_LENGTH =30;
+	
+
+	
+	protected void updateContactId(String contactId) {
+		if(contactId == null) {
+			throw new IllegalArgumentException("Contact ID can not be empty.");
+		} else if (contactId.length() > CONTACT_ID_LENGTH) {
+			throw new IllegalArgumentException("Contact ID can not be longer than " + CONTACT_ID_LENGTH + " characters.");
+		} else {
+			this.contactId = contactId;
+		}
+	}
+	protected void updateFirstName(String firstName) {
+		if (firstName == null) {
+			throw new IllegalArgumentException("First name can not be empty");
+		} else if (firstName.length() > CONTACT_FIRST_NAME_LENGTH) {
+			throw new IllegalArgumentException("First name can not be longer than " + CONTACT_FIRST_NAME_LENGTH + "characters.");
+		} else {
+			this.firstName = firstName;
+		}
+	}
+
+	protected void updateLastName(String lastName) {
+		if (lastName == null) {
+			throw new IllegalArgumentException("Last name can not be null");
+		} else if (lastName.length() > CONTACT_LAST_NAME_LENGTH) {
+			throw new IllegalArgumentException("Last name can not be more than " + CONTACT_LAST_NAME_LENGTH + " characters.");
+		} else {
+			this.lastName = lastName;
+		}
+	}
+
+	protected void updatePhoneNumber(String phoneNumber) {
+		if (phoneNumber == null) {
+			throw new IllegalArgumentException("Phone number can not be empty");
+		} else if (phoneNumber.length() > CONTACT_PHONE_LENGTH) {
+			throw new IllegalArgumentException("Phone number can not be more than " + CONTACT_PHONE_LENGTH + " digits.");
+		} else {
+			this.phoneNumber = phoneNumber;
+		}
+	}
+
+	protected void updateAddress(String address) {
+		if (address == null) {
+			throw new IllegalArgumentException("Address can not be empty");
+		} else if (address.length() > CONTACT_ADDRESS_LENGTH) {
+			throw new IllegalArgumentException("Address can not be more than " + CONTACT_ADDRESS_LENGTH + " characters.");
+		} else {
+			this.address = address;
+		}
+	}
+}
---
+++ src/ContactService.java
@@ -0,0 +1,10 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class ContactService {
+
+	//Comment to create change for git
+	private String uniqueId;
+	private List<Contact> contactList = new ArrayList<>();
+	
+}
