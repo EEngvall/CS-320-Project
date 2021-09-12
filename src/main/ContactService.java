9624bfc7e49d661109dc74d88e6724c8512cde57
Erik Engvall <Erik.Engvall@snhu.edu>
2021-09-11 17:06:41 -0700

    Added unique ID generator
    Updated ContactServiceTest.java to utilize new unique ID

--- src/main/ContactService.java
+++ src/main/ContactService.java
@@ -3,17 +3,24 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
+import org.junit.platform.engine.UniqueId;
+
 public class ContactService {
 
 
 	//Comment to create change for git
-	public static int uniqueId = 0;
+	public String uniqueId;
 	List<Contact> contactList = new ArrayList<>();
 	
+	private String newUniqueId() {
+		return uniqueId = UUID.randomUUID().toString().substring(
+		               0, 10);
+	}
+	
+	
 	public void addContact() {
-		Contact contact = new Contact();
-		String contactIDString = Integer.toString(uniqueId);
-		uniqueId++;
+		Contact contact = new Contact(newUniqueId());
+		contactList.add(contact);
 	}
 	
 	
--- src/test/ContactServiceTest.java
+++ src/test/ContactServiceTest.java
@@ -3,11 +3,18 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 import org.junit.jupiter.api.Assertions;
 
+import main.ContactService;
+
 
 public class ContactServiceTest {
 
 	@Test
-	public void updateContactId() {
+	public void AddNewContactTest() {
+		ContactService service = new ContactService();
+		service.addContact();
+		//Print unique ID to screen to help verify randomness
+		System.out.println(service.uniqueId);
 		
+		assertNotNull(service.uniqueId);
 	}
 }
