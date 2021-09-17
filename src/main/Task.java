a33bb6df9e5a468d7b059cb8cda6fba6e934acba
Erik Engvall <Erik.Engvall@snhu.edu>
2021-09-17 15:21:19 -0700

    Made small update to Task.java

--- src/main/Task.java
+++ src/main/Task.java
@@ -4,22 +4,29 @@ public class Task {
 
 	private String taskName;
 	private String taskDescription;
+	private String uniqueId;
 	
-	private String INITIALIZED_VALUE = "DEFUALT";
+	private String INITIALIZED_VALUE = "DEFAULT";
 	
 	private int TASK_NAME_MAX_LENGTH = 20;
 	private int TASK_DESCRIPTION_MAX_LENGTH = 50;
 	
 	public Task() {
+		this.uniqueId = INITIALIZED_VALUE;
 		this.taskName = INITIALIZED_VALUE;
 		this.taskDescription = INITIALIZED_VALUE;
 		
 	}
-	public Task(String taskName) {
+	public Task(String uniqueId) {
+		this.uniqueId = uniqueId;
+	}
+	public Task(String uniueId, String taskName) {
+		this.uniqueId = uniueId;
 		updateTaskName(taskName);
 		this.taskDescription = INITIALIZED_VALUE;
 	}
-	public Task(String taskName, String taskDescription) {
+	public Task(String uniueId, String taskName, String taskDescription) {
+		this.uniqueId = uniueId;
 		updateTaskName(taskName);
 		updateTaskDescription(taskDescription);
 	}
@@ -49,5 +56,8 @@ public class Task {
 	public String getTaskDescription() {
 		return this.taskDescription;
 	}
-
+	
+	public String getTaskId() {
+		return this.uniqueId;
+	}
 }
--- src/test/TaskTest.java
+++ src/test/TaskTest.java
@@ -9,13 +9,17 @@ import main.Task;
 
 class TaskTest {
 
+	public String uniqueId;
 	public String taskName;
 	public String taskDescription;
 	public String taskNameLong;
 	public String taskDescriptionLong;
 	
+	
+	//A SetUp method to preset the variables each test will use.
 	@BeforeEach
 	public void setUp() {
+		uniqueId = "1234567890";
 		taskName = "Test Task Name";
 		taskDescription = "Task Description";
 		taskNameLong = "This sentence is too long for the task.";
@@ -23,20 +27,41 @@ class TaskTest {
 		System.out.println("Setting up...");
 	}
 
+	//Test for the initial constructor
 	@Test
-	void taskConstructortest() {
-		Task task = new Task(taskName, taskDescription);
+	void constructorTest() {
+		Task task = new Task(uniqueId, taskName, taskDescription);
 		assertAll("Task Constructor Test", 
 				() -> assertEquals(taskName, task.getTaskName()),
 				() -> assertEquals(taskDescription, task.getTaskDescription()));		
 		System.out.println("Constructor Test...");
 	}
 	
+	//Test to verify that the default values are not null when instantiated even without input.
 	@Test
-	void taskNameUpdateTestLength() {
+	public void updateNullTest() {
+		Task task = new Task();
+		assertAll("Contact Null Update",
+				() -> assertNotNull(task.getTaskId()),
+				() -> assertNotNull(task.getTaskName()),
+				() -> assertNotNull(task.getTaskDescription()));
+		System.out.println("Not null test...");
+	}
+	
+	//Test to verify the name meets length requirements
+	@Test
+	void updateTaskNameLengthTest() {
 		Task task = new Task();
 		assertThrows(IllegalArgumentException.class, () -> task.updateTaskName(taskNameLong));
 		System.out.println("Name Length Test...");
+	}
+	
+	//Test to verify description meets length requirements. 
+	@Test
+	void updateTaskDescriptionLengthTest() {
+		Task task = new Task();
+		assertThrows(IllegalArgumentException.class, () -> task.updateTaskDescription(taskDescriptionLong));
+		System.out.println("Description Length Test...");
 	}
 
 }
